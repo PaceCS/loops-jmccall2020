c1cec7962fd936d7e582f55ad224f543180cb550
Programming <programming@us-physics-cart-18.local>
2018-04-25 10:25:15 -0400

    sum35 while complete

--- sum35While.js
+++ sum35While.js
@@ -2,7 +2,26 @@
 // The function outputs the sum of all multiples of 3 and 5 that are less than that number.
 
 function sum35While(N) {
-
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+    let i = 1;
+    while (i < N) {
+        if (i % 3 == 0) {
+            a = a + 1;
+            c = c + i;
+        }
+        else if (i % 5 == 0) {
+            b = b + 1;
+            d = d + i;
+        }
+        i += 1;
+    }
+    const e = a + b;
+    console.log('There are ' + e + ' multiples of 3 and 5 that are less than ' + N + '.');
+    const f = c + d;
+    console.log('Their sum is ' + f + '.');
 }
 
 //Try out your function here.
